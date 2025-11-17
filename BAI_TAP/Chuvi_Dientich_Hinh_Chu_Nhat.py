7ab22eb71e2de940648bc3cea568ae66937a4c38
hienpm67khmt <hien.pm.67khmt@ntu.edu.vn>
2025-11-17 17:00:34 +0700

    Tính Chu Vi Và Diện Tích Hình Chữ Nhật

--- BAI_TAP/Chuvi_Dientich_Hinh_Chu_Nhat.py
+++ BAI_TAP/Chuvi_Dientich_Hinh_Chu_Nhat.py
@@ -2,5 +2,5 @@ a=float(input("Nhập vào chiều dài của Hình Chữ Nhật:"))
 b=float(input("Nhập vào chiều rộng của Hình Chữ Nhật:"))
 cv=(a+b)*2
 dt=a*b
-print("Chu vi hình chữ nhật là",cv)
-print("Diện tích hình chữ nhật là",dt)
+print("Chu vi hình chữ nhật là", int(cv))
+print("Diện tích hình chữ nhật là", int(dt))
