ac3fa97423aa5b95ca49e6604668f8a074f6e584
hienpm67khmt <hien.pm.67khmt@ntu.edu.vn>
2025-11-26 10:12:20 +0700

    Tính phần trăm

---
+++ BAI_TAP/TINH_PHAN_TRAM.py
@@ -0,0 +1,6 @@
+a = int(input("Nhập vào số bạn muốn tính %: "))
+b = float(input("Nhập vào phần trăm bạn muốn tính cho số đó: "))
+
+phan_tram = a * b / 100
+
+print(f"Phần trăm của {b}% của số {a} là {phan_tram}")
