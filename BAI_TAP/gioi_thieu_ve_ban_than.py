647542bfd81ae3f4b717c426763ae98c34161277
hienpm67khmt <hien.pm.67khmt@ntu.edu.vn>
2025-11-26 09:54:10 +0700

    Giới thiệu về bản thân

---
+++ BAI_TAP/gioi_thieu_ve_ban_than.py
@@ -0,0 +1,4 @@
+ten=input("Nhập vào họ tên của bạn :")
+tuoi=int(input("Nhập vào tuổi của bạn:"))
+que_quan=input("Nhập vào quê quán của bạn:")
+print("Xin chào, tôi tên là",ten,",",tuoi,"tuổi",",","đến từ",que_quan)
